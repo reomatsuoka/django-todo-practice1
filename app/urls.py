dfa6c09adbbd825ac1e41d8e448f9a9f0bd16cc5
reomatsuoka <reo199637458@gmail.com>
2020-12-01 00:57:51 +0900

    practice2

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns =[
   path('', views.IndexView.as_view(), name='index'),
+  path('<int:pk>/edit/', views.EditView.as_view(), name='edit'),
+  path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import View
 from .models import Todo
+from .forms import TodoForm
 
 class IndexView(View):
   def get(self, request, *args, **kwargs):
     todo_data = Todo.objects.order_by('deadline')
+    form = TodoForm(request.POST or None)
 
     return render(request, 'app/index.html',{
-      'todo_data' : todo_data
+      'todo_data' : todo_data,
+      'form' : form,
     })
+
+  def post(self, request, *args, **kwargs):
+    form = TodoForm(request.POST or None)
+
+    if form .is_valid():
+      todo_data = Todo()
+      todo_data.title = form.cleaned_data['title']
+      todo_data.deadline = form.cleaned_data['deadline']
+      todo_data.save()
+    return redirect('index')
+
+class EditView(View):
+  def get(self, request, *args, **kwargs):
+    todo_data = Todo.objects.get(id=self.kwargs['pk'])
+    form = TodoForm(
+      request.POST or None,
+      initial={
+        'title':todo_data.title,
+        'deadline':todo_data.deadline,
+      })
+
+    return render(request, 'app/edit.html',{
+      'form': form
+    })
+  def post(self, request, *args, **kwargs):
+    form = TodoForm(request.POST or None)
+
+    if form .is_valid():
+      todo_data = Todo.objects.get(id=self.kwargs['pk'])
+      todo_data.title = form.cleaned_data['title']
+      todo_data.deadline = form.cleaned_data['deadline']
+      todo_data.save()
+    return redirect('index')
+
+    return redirect('edit', self.kwargs['pk'])
+
+class DeleteView(View):
+  def get(self, request, *args, **kwargs):
+    todo_data = Todo.objects.get(id=self.kwargs['pk'])
+    todo_data.delete()
+    return redirect('index')
+    
