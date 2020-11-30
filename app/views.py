3f7f0c06d9e62bbe73065b60bb8681222d386c0c
reomatsuoka <reo199637458@gmail.com>
2020-11-30 22:52:42 +0900

    practice

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .models import Todo
+
+class IndexView(View):
+  def get(self, request, *args, **kwargs):
+    todo_data = Todo.objects.order_by('deadline')
+
+    return render(request, 'app/index.html',{
+      'todo_data' : todo_data
+    })
